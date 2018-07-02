c4c24fee921f1012d3ed1648146f47652f103bac
kunrad <duruconrad@gmail.com>
2018-07-02 12:10:12 +0100

    First version pf application with the login authenticated
    Signed-off-by: kunrad <duruconrad@gmail.com>

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('username');
+            $table->Integer('phone');
+            $table->integer('meterno');
+            $table->string('password');
+            $table->string('activation_code')->nullable();
+            $table->boolean('status')->default(0);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
